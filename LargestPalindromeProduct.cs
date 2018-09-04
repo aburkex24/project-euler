1ee56e312116a92c5e025b9d928503bcae944199
Andre Burke <aburke@Andres-MacBook-Pro.local>
2018-09-03 22:20:31 -0600

    adding private members and two methods euler # four

---
+++ LargestPalindromeProduct.cs
@@ -0,0 +1,27 @@
+namespace ProjectEuler 
+{
+    public class LargestPalindromeProduct
+    {
+        private short _leftOperand; 
+        private short _rightOperand;
+        private int _palindrome;
+
+        public LargestPalindromeProduct()
+        {   
+            _leftOperand = 999;
+            _rightOperand = 999;
+            _palindrome = 0;
+        }
+
+        public int FindPalindrome()
+        {
+            return 0;   
+        }
+
+        private bool IsPalindrome()
+        {
+            
+            return false;
+        }
+    }
+}
